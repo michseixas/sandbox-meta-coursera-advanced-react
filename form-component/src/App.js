ab42b7d1922cd51002b9e2341348b62a7f5e6bab
Mich Seixas <mcseixas@gmail.com>
2023-11-02 19:05:56 +0100

    controled form components

--- form-component/src/App.js
+++ form-component/src/App.js
@@ -4,17 +4,24 @@ import "./App.css";
 function App() {
   const [name, setName] = useState("");
 
-  const handleSubmit = () => {
-    console.log("Form submitted!");
+  const handleSubmit = (e) => {
+    //preventing requests to server:
+    e.preventDefault();
+    //clear the input after submission:
+    setName("");
+    console.log("Form submitted!", console.log);
   };
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <fieldset>
+        <div> 
           <div className="Field">
-            <label>Name:</label>
+          {/* connect the label with the input, by setting the id and htmlFor */}
+            <label htmlFor="name">Name:</label>
             <input
+            id="name"
               type="text"
               placeholder="Name"
               name="name"
@@ -22,7 +29,9 @@ function App() {
               onChange={(e) => setName(e.target.value)}
             />
           </div>
-          <button type="submit">Submit</button>
+          {/* button disabled if no name provided */}
+          <button disabled={!name} type="submit">Submit</button>
+          </div>
         </fieldset>
       </form>
     </div>
