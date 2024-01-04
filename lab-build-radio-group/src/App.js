a1f7a44be817212709f992750ff842cc9d6eb9c5
Mich Seixas <mcseixas@gmail.com>
2024-01-04 22:05:20 +0100

    solved

--- lab-build-radio-group/src/App.js
+++ lab-build-radio-group/src/App.js
@@ -55,3 +55,35 @@ export default App;
 // RadioGroup component to translate the selected prop to the checked prop that each RadioOption child receives.
 // }
 
+
+//Solution
+// Step 2
+
+// The RadioOptions variable should be assigned to the return value of React.Children.map, which will be a new React element. 
+// The first argument passed to the map function should be the children prop, and the second is a function that gets invoked 
+// in every child contained within the children property. Recall that a children prop is a special prop all React components 
+// have and that it presents a special data structure, similar to arrays, where you can perform iterations. However, they are 
+// not exactly instances of JavaScript arrays. That’s why to iterate over all siblings you should use the special React.children.map 
+// API provided by React.
+
+// Inside the map projection function, you should first clone the element using React.cloneElement, passing as first argument 
+// the target child element and as a second argument a configuration with all new props. The resulting element will have the original 
+// element’s props with the new props merged in.
+
+// onChange can be passed to each child (RadioOption) as it is and checked is the property the RadioOption uses to determine if 
+// the underlying radio input is selected. Since RadioGroup receives a selected property, which is a string pointing to the value of the 
+// option that has been selected, checked will be only true for one of the options at any point in time. This is guaranteed by performing 
+// an equality check, comparing the RadioOption value prop with the selected value.
+
+// Finally, the RadioGroup component returns the new RadioOptions elements by wrapping them in curly braces.
+
+// Step 3
+
+// The RadioOption component now receives two new props implicitly, onChange and checked, that RadioGroup is injecting via children manipulation,
+//  as seen in the previous section.
+
+// The value prop is already provided explicitly inside the App.js component and children represents the label text for the radio input. 
+
+// You have to connect the props value, checked and onChange correctly. First, both value and checked props should be passed to the radio input as is. 
+// Then, you should use the onChange event from the radio input, retrieve the value property from the event target object and pass it to the onChange 
+// prop as the argument as seen below. That completes the implementation of the RadioOption component.
