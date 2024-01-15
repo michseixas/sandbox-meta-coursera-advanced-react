1495da6b3a636e5f17008912e8e19d7017021615
Mich Seixas <mcseixas@gmail.com>
2024-01-15 22:03:23 +0100

    Step 2 - landing section done

--- final-advanced-react-lab/src/components/LandingSection.js
+++ final-advanced-react-lab/src/components/LandingSection.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Avatar, Heading, VStack } from "@chakra-ui/react";
 import FullScreenSection from "./FullScreenSection";
+import './LandingSection.css';
 
 const greeting = "Hello, I am Pete!";
 const bio1 = "A frontend developer";
@@ -15,6 +16,12 @@ const LandingSection = () => (
     isDarkBackground
     backgroundColor="#2A4365"
   >
+    <img className="profile" src="https://i.pravatar.cc/150?img=7" alt="Girl in a jacket" />
+    <p>{greeting}</p>
+    <br></br>
+
+    <p className="developer">{bio1}</p>
+    <p className="developer">{bio2}</p>
 
   </FullScreenSection>
 );
